3e40b45b1f681609a671b5b5fc7bee522f23bb37
Deepan360 <deepandeep686@gmail.com>
2024-06-20 16:53:42 +0530

    col

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,12 @@
 apt-get update
 
 # Install system dependencies
-apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev g++ make
+
+# Ensure python is available for node-gyp
+apt-get install -y python3
+
+# Link python3 to python if necessary
+ln -s /usr/bin/python3 /usr/bin/python || true
 
 # Other dependencies can be added here
